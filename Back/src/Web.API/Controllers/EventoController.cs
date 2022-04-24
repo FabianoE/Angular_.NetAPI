c68080947b6687657fe18617fb229bdbbcbde4a6
Fabiano Evaristo <gameplaysfabiano@gmail.com>
2022-04-24 00:36:10 -0300

    EF Core com Sqlite

--- Back/src/Web.API/Controllers/EventoController.cs
+++ Back/src/Web.API/Controllers/EventoController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Web.API.Data;
 using Web.API.Models;
 
 namespace Web.API.Controllers
@@ -11,61 +12,24 @@ namespace Web.API.Controllers
     [ApiController]
     [Route("api/[controller]")]
     public class EventoController : ControllerBase
-    {
-        public IEnumerable<Evento> _evento = new Evento[]{
-                new Evento(){
-                    EventoId = 1,
-                    Tema = "Angular e  .Net 5",
-                    Local = "BH",
-                    Lote = "1 Lote",
-                    QtdPessoas = 250,
-                    DataEvento = DateTime.Now.AddDays(2).ToString(),
-                    ImagemURL = "VAZIO"
-                },
-                new Evento(){
-                    EventoId = 2,
-                    Tema = "Angular e  .Net 5",
-                    Local = "SP",
-                    Lote = "2 Lote",
-                    QtdPessoas = 150,
-                    DataEvento = DateTime.Now.AddDays(5).ToString(),
-                    ImagemURL = "VAZIO"
-                }
-        };
+    {   
+        private readonly DataContext context;
 
-        public EventoController()
+        public EventoController(DataContext context)
         {
-
+            this.context = context;
         }
 
         [HttpGet]
         public IEnumerable<Evento> Get()
         {
-            return _evento;
+            return context.Eventos;
         }
-        
+
         [HttpGet("{id}")]
-        public IEnumerable<Evento> GetById(int id)
+        public Evento GetById(int id)
         {
-            return _evento.Where(x => x.EventoId == id);
-        }
-
-        [HttpPost]
-        public string Post()
-        {
-            return "Return Post";
-        }
-
-        [HttpPut("{id}")]
-        public string Put(int id)
-        {
-            return "Return Put " + id;
-        }
-
-        [HttpDelete("{id}")]
-        public string DeletHttpDelete(int id)
-        {
-            return "Return Delete " + id;
+            return context.Eventos.FirstOrDefault(x => x.EventoId == id);
         }
     }
 }
