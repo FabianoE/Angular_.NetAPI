ad9b0185a2792aea692e9a5de68b9391ac16effe
Fabiano Evaristo <gameplaysfabiano@gmail.com>
2022-04-24 22:38:00 -0300

    Angular>API

--- Front/Angular-App/src/app/eventos/eventos.component.ts
+++ Front/Angular-App/src/app/eventos/eventos.component.ts
@@ -7,21 +7,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./eventos.component.scss'],
 })
 export class EventosComponent implements OnInit {
-  public eventos: any;
+  public eventos: any = [];
+  public eventosFiltrados: any = [];
+  isCollapsed = true; //Mostrar imagem
+  private _filtroLista: string = '';
 
+  public get filtroLista() {
+    return this._filtroLista;
+  }
+
+  public set filtroLista(value: string) {
+    this._filtroLista = value;
+    this.eventosFiltrados = this.filtroLista? this.filtrarEventos(this.filtroLista): this.eventos;
+  }
 
   constructor(private http: HttpClient) {}
+
+  filtrarEventos(filtro: string): any {
+    filtro = filtro.toLocaleLowerCase();
+    return this.eventos.filter(
+      (evento: any) =>
+        evento.tema.toLocaleLowerCase().indexOf(filtro) !== -1 ||
+        evento.local.toLocaleLowerCase().indexOf(filtro) !== -1
+    );
+  }
 
   ngOnInit() {
     this.getEventos();
   }
 
-  public getEventos(): void{
+  public getEventos(): void {
     this.http.get('https://localhost:5001/api/eventos').subscribe(
-      response => this.eventos = response,
-      error => console.log(error)
+      (response) => {
+        this.eventos = response;
+        this.eventosFiltrados = response;
+      },
+      (error) => console.log(error)
     );
-
-    
   }
 }
